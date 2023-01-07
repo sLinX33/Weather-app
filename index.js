f18cd25bea7206797a42221bd21ffd2aa8b651ee
Sadim Mali <sadimmalakar77@gmail.com>
2023-01-07 19:18:28 +0545

    fetch

---
+++ index.js
@@ -0,0 +1,22 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8b0de5cfe9mshf411bde44fedd5fp13bb99jsn0a5cb11cd495',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+let randomCountry = 'Kathmandu';
+let url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${randomCountry}`;
+
+fetch(url, options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err))
+
+// fetch(url, options)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+
+
+
