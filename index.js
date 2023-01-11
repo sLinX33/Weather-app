0931bb038cd32d6fbe9f4a359f34e340801fb42f
Sadim Mali <sadimmalakar77@gmail.com>
2023-01-11 21:11:01 +0545

    Added a response to its value

--- index.js
+++ index.js
@@ -10,9 +10,20 @@ let url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${random
 
 fetch(url, options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then((response) => {
+
+		console.log(response)
+
+	cloud_pct.innerHTML = response.cloud_pct 
+	feels_like.innerHTML = response.feels_like
+	humidity.innerHTML = response.humidity
+	max_temp.innerHTML = response.max_temp
+	min_temp.innerHTML = response.min_temp
+	sunrise.innerHTML = response.sunrise
+	sunset.innerHTML = response.sunset
+	temp.innerHTML = response.temp
+	wind_degrees.innerHTML = response.wind_degrees
+	wind_speed.innerHTML = response.wind_speed
+	})
 	.catch(err => console.error(err))
-
-
-
 
