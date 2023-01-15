01512dff3cb4145f56b6c4790cdc5fe327107de8
Sadim Mali <106245875+sLinX33@users.noreply.github.com>
2023-01-15 20:39:08 +0545

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const getWeather = (city)=> {
 .catch(err => console.error(err))
 };
 
-// const submit = document.getElementById("submit");
 submit.addEventListener("click", (e)=> {
 	e.preventDefault();
 	getWeather(city.value);
